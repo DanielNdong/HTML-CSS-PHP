7df8de0aa46ac8183c2f235635a7128b7738d58d
AQUILINO <87017154+Berbany01@users.noreply.github.com>
2021-10-19 22:58:31 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,246 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Universidad Nacional de Guinea Ecuatorial</title>
+    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap-utilities.css">
+  </head>
+  <body>
+    <header>
+      <div class="row d-flex barra-info-covid text-center  text-white align-content-center" style="background: #AF0505; height:25px;">
+        <span>
+          <strong>La respuesta más reciente de la unge al Covid-19. <a href="#">Leer más</a> </strong>
+        </span>
+      </div>
+      <div class="row pt-2 pb-2">
+        <div class="col-12 d-flex justify-content-center" style="height:90px">
+          <div class="col-4 ms-5 nombre-uni p-4 d-flex justify-content-center">
+          <strong><h6 style="font-size:19px">UNIVERSIDAD NACIONAL<br>DE GUINEA ECUATORIAL</h6></strong>
+          </div>
+          <div class="col-4 logo-uni d-flex justify-content-center">
+            <img src="IMAGES/ungelogo.jpg" style="width: 85px; height: 90px;" alt="escudo Universidad">
+          </div>
+          <div class="col-4 lema-uni p-4">
+            <h6 style="font-size:13px">UNIVERSIDAD PARA TODOS,<br>INCLUSO PARA PERSONAS MÁS SIMPLE</h6>
+          </div>
+        </div>
+      </div>
+      <div class="img-encabezado">
+        <div class="row">
+          <div class="col-12 ">
+            <div class="menu-encabezado d-flex ms-4 ">
+              <div class="col-4 p-4 d-flex justify-content-center align-content-center">
+                <div class="universidad-option d-block" style="width:125px;">
+                  <input type="checkbox" id="uni">
+                   <label class="unieff" for="uni" >La Univeridad</label>
+                     <div class="texto-universidad">
+                      <ul>
+                        <br>
+                        <li class="mb-2"> <a href="#">Estructura organizativa</a> </li>
+                        <li class="mb-2"> <a href="#">Becas</a> </li>
+                        <li class="mb-2"> <a href="#">Rectorado</a> </li>
+                        <li> <a href="#">Residencia</a> </li>
+                      </ul>
+                     </div>
+                  </div>
+                  <div class="facultad-option d-block mx-3" style="width:94px;">
+                    <input type="checkbox" id="fac">
+                     <label class="unieff" for="fac" style="color:#fff;">Facultades</label>
+                       <div class="texto-facultades">
+                        <ul>
+                          <br>
+                          <li class="mb-2"> <a href="#">Medio ambiente</a> </li>
+                          <li class="mb-2"> <a href="#">Letras y Cienias Sociales</a> </li>
+                          <li class="mb-2"> <a href="#">Ciencias Econmicas, Gestion y Administración</a> </li>
+                          <li class="mb-2"> <a href="#">Ciencias de la Informaición y Filologia Linguistica</a> </li>
+                          <li> <a href="#">Ciencias de la Salud</a> </li>
+                        </ul>
+                       </div>
+                    </div>
+                    <div class="cursos-option d-block">
+                      <input type="checkbox" id="cur">
+                       <label class="unieff" for="cur" style="color:#fff;">Cursos</label>
+                         <div class="texto-cursos">
+                          <ul>
+                            <br>
+                            <li class="mb-2"> <a href="#">Chino</a> </li>
+                            <li class="mb-2"> <a href="#">Ingles</a> </li>
+                            <li class="mb-2"> <a href="#">Portugués</a> </li>
+                            <li class=""> <a href="#">Español</a> </li>
+                          </ul>
+                         </div>
+                      </div>
+                    </div>
+              <div class="siglas col-4 justify-content-center">
+                <div class=" text-white text-center">
+                    <h2 style="font-size: 80px;">UNGE</h2>
+                    <div class="ms-1 d-flex justify-content-center">
+                        <div class="verde">
+                        </div>
+                        <div class="blanco">
+                        </div>
+                        <div class="rojo">
+                        </div>
+                        <div class="azul">
+                          </div>
+                      </div>
+                </div>
+              </div>
+              <div class="col-4 p-4 d-flex justify-content-start alig-content-center" >
+                <div class="universidad-option d-block" style="width:140px;">
+                  <input type="checkbox" id="vc">
+                   <label class="unieff" for="vc" style="color:#fff;">El Campus</label>
+                     <div class="texto-campus">
+                      <ul>
+                        <br>
+                        <li class="mb-2"> <a href="#">Alumnos</a> </li>
+                        <li class="mb-2"> <a href="#">Biblioteca</a> </li>
+                        <li class="mb-2"> <a href="#">Vida en el campus</a> </li>
+                        <li> <a href="#">Semana de la UNGE</a> </li>
+                      </ul>
+                     </div>
+                  </div>
+                  <div class="facultad-option d-block mx-3" style="width:80px;">
+                    <input type="checkbox" id="mtc">
+                     <label class="unieff" for="mtc" style="color:#fff;">Matricula</label>
+                       <div class="texto-matricula">
+                        <ul>
+                          <br>
+                          <li class="mb-2"> <a href="#">Alumnos Nuevos</a> </li>
+                          <li class="mb-2"> <a href="#">Registros</a> </li>
+                          <li class="mb-2"> <a href="#">Solicitar plaza</a> </li>
+                        </ul>
+                       </div>
+                    </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+<section>
+      <div class="container ">
+       <div class="d-block mb-5 justify-content-center">
+        <div class="col-12 d-grid justify-content-center">
+          <div class="type-title d-flex justify-content-center align-items-center">
+             <div class=" mt-2 d-flex justify-content-center">
+            <h4>Cursos</h4>
+          </div>
+          </div>
+        </div>
+        <div class="type-card mt-4 ">
+          <div class="col-12 d-flex justify-content-center">
+          <div class="ajustar d-flex" style="width: 800px;">
+            <div class="col-4">
+              <div class="card" style="width: 230px; height: 380px;">
+                <img class="card-img-top" src="IMAGES/colectivo.jpg" alt="gente estudiante">
+              <div class="card-body text-white d-flex align-items-center">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor equatat laborum.</p>
+              </div>
+              </div>
+            </div>
+            <div class="col-4">
+              <div class="card" style="width: 15rem;">
+                <img class="card-img-top" src="IMAGES/pose.jpg" alt="gente estudiante">
+              <div class="card-body text-white d-flex align-items-center">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor equatat laborum.</p>
+              </div>
+              </div>
+            </div>
+            <div class="col-4">
+              <div class="card" style="width: 15rem;">
+                <img class="card-img-top" src="IMAGES/biblioteca.jpg" alt="gente estudiante">
+               <div class="card-body text-white d-flex align-items-center">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt uborum.</p>
+              </div>
+              </div>
+            </div>
+          </div>
+          </div>
+        </div>
+       </div>
+       <div class="enlace1 text-center pt-5">
+         <a href="#">Más informacion acerca de cursos</a>
+       </div>
+      </div>
+
+
+    </section>
+
+    <div class="row">
+      <div class="galeria">
+        <div class="container1 d-flex justify-content-center align-items-center">
+          <button type="button" class="img-btn" name="button"> <a href="#"> <strong>Orientaciones</strong> </button>
+        </div>
+        <div class="container2 d-flex justify-content-center align-items-center">
+          <button type="button" class="img-btn" name="button"> <a href="#"> <strong>Trabajos <br> de fin de grado</strong> </a></button>
+        </div>
+        <div class="container3 d-flex justify-content-center align-items-center">
+          <button type="button" class="img-btn" name="button"><a href="#"> <strong>Programas <br> de ayuda</strong> </a></button>
+        </div>
+        <div class="container4 d-flex justify-content-center align-items-center">
+          <button type="button" class="img-btn" name="button"> <a href="#"> <strong>Eventos</strong> </a> </button>
+        </div>
+        <div class="container5 d-flex justify-content-center align-items-center">
+          <button type="button" class="img-btn" name="button"> <a href="#"> <strong>Seminarios</strong> </a> </button>
+        </div>
+      </div>
+    </div>
+
+    <div class="img-cover"></div>
+
+    <div class="container-facult mt-3 ">
+      <div class="subcontenedor col-12 d-flex justify-content-center">
+        <div class="col-7 d-flex justify-content-center text-center">
+        <div class="zone-facult">
+          <div class="mt-2 d-flex justify-content-center" >
+            <div class="type-title mb-2">
+              <h4>Facultades</h4>
+             </div>
+           </div>
+         <div class="area-fac">
+            <div class="row subarea-fac d-block p-3">
+              <div class="tips-fac1 d-flex" >
+                <div class=" text d-block mb-3 justify-content-start">
+                  <div class="fac-tittle">MEDIO AMBIENTE</div>
+                  <div class="text-description mt-2 d-flex justify-content-start">
+                    <p> <strong>Lorem ipsum doloiusmodstrud exe ex eaquat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culst laborum.</strong> </p>
+                  </div>
+                </div>
+                <div class="img ms-4">
+                  <img class="img-description" src="IMAGES/analistas.jpg" alt="">
+                </div>
+              </div>
+              <div class="tips-fac2 mt-3 d-flex">
+                <div class="img">
+                  <img class="img-description" src="IMAGES/reunion.jpg" alt="">
+                </div>
+                <div class=" text d-block ms-4 mb-3 ">
+                  <div class="fac-tittle mb-3">DERECHO</div>
+                  <div class="text-description">
+                      <p> <strong>Lorem ipsum doloiusmodstrud exe ex eaquat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cuident,lst laborum.</strong> </p>
+                  </div>
+                </div>
+              </div>
+              <div class="tips-fac3 ms-2 mt-4 d-flex justify-content-center">
+                <div class="fac-tittleE mb-3 p-5 mt-3"> <a href="#" style="text-decoration: none;">INFORMATICA DE GESTIÓN</a> </div>
+                <div class=" text-description ms-3 mb-3 d-flex align-items-center">
+                    <p> <strong>Lorem ipsum doloiusmodstrud exe irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culst laborum.</strong> </p>
+                </div>
+                <div class="img">
+                  <img class="img-descriptionE" src="IMAGES/reunion.jpg" alt="">
+                </div>
+              </div>
+            </div>
+         </div>
+       </div>
+
+        </div>
+      </div>
+    </div>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/bootstrap.bundle.js"></script>
+  </body>
+</html>
